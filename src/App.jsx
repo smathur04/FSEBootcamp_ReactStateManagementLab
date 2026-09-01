1ca1617b22b9d89e73cd4974708c97534f1c38d7
mathur shaan <du_353718-1787851001@U-1DMH9WNBJ5PJK.me.ga.co>
2026-09-01 14:27:05 -0700

    Getting started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Zombie Fighters</h1>
+      <FighterStats />
+      <FighterMenu />
+    </>
+  );
+};
+
+export default App;
